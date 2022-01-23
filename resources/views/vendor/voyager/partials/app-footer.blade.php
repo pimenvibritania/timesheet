0b35763bd74065139d49fefa4c6b985520c2fd9d
pimenvibritania <pimenvibritania@gmail.com>
2022-01-24 00:47:54 +0700

    footer

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        Made with <i class="voyager-heart"></i> by <a href="https://doogether.id" target="_blank">DOOgether</a>
+    </div>
+</footer>
